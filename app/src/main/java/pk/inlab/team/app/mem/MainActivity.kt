5aaf0c622a41ef162a13dbe12999b0e963a836f8
arshay_super_user <arshadalisoomro7@gmail.com>
2021-12-05 12:43:17 +0500

    Added InputDialogFragment with basic inputs

--- app/src/main/java/pk/inlab/team/app/mem/MainActivity.kt
+++ app/src/main/java/pk/inlab/team/app/mem/MainActivity.kt
@@ -9,9 +9,11 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import pk.inlab.team.app.mem.databinding.ActivityMainBinding
+import pk.inlab.team.app.mem.ui.views.InputDialogFragment
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,9 +38,10 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        binding.fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.fab.setOnClickListener {
+            showDialog()
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
         }
     }
 
@@ -62,5 +65,13 @@ class MainActivity : AppCompatActivity() {
         navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration)
                 || super.onSupportNavigateUp()
+    }
+
+    private fun showDialog() {
+        val fragmentManager = supportFragmentManager
+        val inputDialogFragment = InputDialogFragment(getString(R.string.new_item))
+
+        inputDialogFragment.show(fragmentManager, "dialog")
+
     }
 }
---
+++ app/src/main/java/pk/inlab/team/app/mem/ui/views/InputDialogFragment.kt
@@ -0,0 +1,40 @@
+package pk.inlab.team.app.mem.ui.views
+
+import android.app.Dialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import pk.inlab.team.app.mem.R
+
+class InputDialogFragment(private val title: String) : DialogFragment() {
+
+    /** The system calls this to get the DialogFragment's layout, regardless
+    of whether it's being displayed as a dialog or an embedded fragment. */
+
+    companion object{
+        const val DIALOG_TITLE = "title"
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        savedInstanceState?.putString(DIALOG_TITLE, title)
+        // Inflate the layout to use as dialog or embedded fragment
+        return inflater.inflate(R.layout.purchase_item, container, false)
+    }
+
+    /** The system calls this only when creating the layout in a dialog. */
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        // The only reason you might override this method when using onCreateView() is
+        // to modify any dialog characteristics. For example, the dialog includes a
+        // title by default, but your custom layout might not need it. So here you can
+        // remove the dialog title, but you must call the superclass to get the Dialog.
+        val dialog = super.onCreateDialog(savedInstanceState)
+        dialog.setTitle(savedInstanceState?.getString(DIALOG_TITLE))
+        return dialog
+    }
+}
