07af8a3c8052c2b3d0323a6a8429f06ed4c501ad
arshay_super_user <arshadalisoomro7@gmail.com>
2021-12-07 19:35:48 +0500

    Added Edit Delete Popup menu in CurrentMonthAdapter.kt

--- app/src/main/java/pk/inlab/team/app/mem/adapter/CurrentMonthAdapter.kt
+++ app/src/main/java/pk/inlab/team/app/mem/adapter/CurrentMonthAdapter.kt
@@ -4,11 +4,14 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.MenuRes
+import androidx.appcompat.widget.PopupMenu
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textview.MaterialTextView
 import pk.inlab.team.app.mem.R
 import pk.inlab.team.app.mem.databinding.CurrentItemInfoBinding
@@ -37,9 +40,16 @@ class CurrentMonthAdapter(private val rootView: View) :
             val item = getItem(position)
             holder.itemWeight.text = item.purchaseWeight.toString()
 
-            // Click listener
+            // Click listener to show Info
             holder.itemContainer.setOnClickListener {
                 showItemInfoDialog(context, item)
+            }
+
+            // Long Click listener to show Edit Delete Popup menu
+            holder.itemContainer.setOnLongClickListener {
+                showEditDeletePopUpMenu(rootView, it, R.menu.menu_pop_up)
+
+                return@setOnLongClickListener true
             }
 
         }
@@ -77,6 +87,33 @@ class CurrentMonthAdapter(private val rootView: View) :
             .show()
     }
 
+    private fun showEditDeletePopUpMenu(rootView: View, v: View, @MenuRes menuRes: Int) {
+        val popup = PopupMenu(v.context, v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+
+        popup.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.option_edit -> {
+                    Snackbar.make(rootView, "Edit Clicked", Snackbar.LENGTH_SHORT)
+                        .show()
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.option_delete -> {
+                    Snackbar.make(rootView, "Delete Clicked", Snackbar.LENGTH_SHORT)
+                        .show()
+                    return@setOnMenuItemClickListener true
+                }
+                else -> return@setOnMenuItemClickListener false
+            }
+
+            // return@setOnMenuItemClickListener true
+        }
+        popup.setOnDismissListener {
+            // Respond to popup being dismissed.
+        }
+        // Show the popup menu.
+        popup.show()
+    }
 
     inner class CurrentMonth(binding: ItemCurrentBinding) :
             RecyclerView.ViewHolder(binding.root) {
