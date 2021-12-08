7601850193bc03e8f4bcc77041424495155f6f37
arshay_super_user <arshadalisoomro7@gmail.com>
2021-12-08 11:13:04 +0500

    Refactored SettingsFragment.kt for only numbers to be input

--- app/src/main/java/pk/inlab/team/app/mem/ui/settings/SettingsFragment.kt
+++ app/src/main/java/pk/inlab/team/app/mem/ui/settings/SettingsFragment.kt
@@ -1,6 +1,8 @@
 package pk.inlab.team.app.mem.ui.settings
 
 import android.os.Bundle
+import android.text.InputType
+import androidx.preference.EditTextPreference
 import androidx.preference.PreferenceFragmentCompat
 import pk.inlab.team.app.mem.R
 
@@ -8,5 +10,13 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.root_preferences, rootKey)
+
+        val currentRatePerKiloEditTextPreference : EditTextPreference? = findPreference("rate_per_kilo")
+
+        currentRatePerKiloEditTextPreference?.setOnBindEditTextListener { editText ->
+            editText.inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_DECIMAL
+
+        }
+
     }
 }
