88f74050a907725f57a5cae7257873c8cee62204
arshay_super_user <arshadalisoomro7@gmail.com>
2021-12-08 10:22:22 +0500

    Added Popup menu to HistoryAdapter.kt

--- app/src/main/java/pk/inlab/team/app/mem/adapter/HistoryAdapter.kt
+++ app/src/main/java/pk/inlab/team/app/mem/adapter/HistoryAdapter.kt
@@ -1,14 +1,22 @@
 package pk.inlab.team.app.mem.adapter
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.graphics.drawable.InsetDrawable
+import android.os.Build
+import android.util.TypedValue
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.MenuRes
+import androidx.appcompat.view.menu.MenuBuilder
+import androidx.appcompat.widget.PopupMenu
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textview.MaterialTextView
 import pk.inlab.team.app.mem.R
 import pk.inlab.team.app.mem.databinding.HistoryItemInfoBinding
@@ -44,6 +52,13 @@ class HistoryAdapter(private val rootView: View):
         holder.itemContainer.setOnClickListener {
             showItemInfoDialog(context, item)
         }
+
+
+        // Long Click listener to show Edit Delete Popup menu
+        holder.itemContainer.setOnLongClickListener {
+            showEditDeletePopUpMenu(rootView, it, R.menu.menu_pop_up_history)
+            return@setOnLongClickListener true
+        }
     }
 
     private fun showItemInfoDialog(context: Context, item: PurchaseItem) {
@@ -77,6 +92,58 @@ class HistoryAdapter(private val rootView: View):
                 dialog.dismiss()
             }
             .show()
+    }
+
+    @SuppressLint("RestrictedApi")
+    private fun showEditDeletePopUpMenu(rootView: View, v: View, @MenuRes menuRes: Int) {
+        val popup = PopupMenu(v.context, v)
+        popup.menuInflater.inflate(menuRes, popup.menu)
+
+        popup.setOnMenuItemClickListener {
+            when(it.itemId){
+                R.id.option_edit -> {
+                    Snackbar.make(rootView, "Edit Clicked", Snackbar.LENGTH_SHORT)
+                        .show()
+                    return@setOnMenuItemClickListener true
+                }
+                R.id.option_delete -> {
+                    Snackbar.make(rootView, "Delete Clicked", Snackbar.LENGTH_SHORT)
+                        .show()
+                    return@setOnMenuItemClickListener true
+                }
+                else -> return@setOnMenuItemClickListener false
+            }
+
+        }
+
+        if (popup.menu is MenuBuilder) {
+            val menuBuilder = popup.menu as MenuBuilder
+            menuBuilder.setOptionalIconsVisible(true)
+            for (item in menuBuilder.visibleItems) {
+
+                val ICON_MARGIN = 16
+
+                val iconMarginPx =
+                    TypedValue.applyDimension(
+                        TypedValue.COMPLEX_UNIT_DIP, ICON_MARGIN.toFloat(), rootView.resources.displayMetrics)
+                        .toInt()
+                if (item.icon != null) {
+                    if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+                        item.icon = InsetDrawable(item.icon, iconMarginPx, 0, iconMarginPx,0)
+                    } else {
+                        item.icon =
+                            object : InsetDrawable(item.icon, iconMarginPx, 0, iconMarginPx, 0) {
+                                override fun getIntrinsicWidth(): Int {
+                                    return intrinsicHeight + iconMarginPx + iconMarginPx
+                                }
+                            }
+                    }
+                }
+            }
+        }
+
+        // Show the popup menu.
+        popup.show()
     }
 
     inner class History(binding: ItemHistoryBinding) :
